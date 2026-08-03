37283e12729c021a0b12c5c51e2c6455bb5f0969
Caitlin Gilbert <caitlin.gilbert@washpost.com>
2026-08-02 21:05:13 -0400

    fix code to update to s10 cast page

--- loveisblind.R
+++ loveisblind.R
@@ -2,46 +2,36 @@ library(tidyverse)
 #library(vroom)
 library(rvest)
 
-### s6
-s6page <- read_html("https://www.netflix.com/tudum/articles/love-is-blind-season-6-cast-instagrams")
+### s10
+s10page <- read_html("https://www.netflix.com/tudum/features/love-is-blind-season-10-cast-instagrams")
 
-age_jobs_s6 <- s6page %>%
-  html_elements(".e1l1jp228+ .e1l1jp226") %>%
-  html_text()
+card_nodes <- s10page %>%
+  html_elements(".default-ltr-iqcdef-cache-0.eoienyz7")
 
-extraagejob_s6 <- s6page %>%
-  html_element("hr+ .e1l1jp227 .e1l1jp228+ .e1l1jp226") %>%
-  html_text()
+cast_df <- tibble(
+  name = card_nodes %>% 
+    html_element("h3[data-sel='heading']") %>% 
+    html_text2() %>% 
+    str_squish(),
+  age = card_nodes %>% 
+    html_elements("div[data-sel='sub-head-2']") %>% 
+    .[html_text2(.) == "Age"] %>% 
+    html_element(xpath = "following-sibling::text()") %>% 
+    html_text() %>% 
+    str_squish() %>% 
+    as.integer(),
+  occupation = card_nodes %>% 
+    html_elements("div[data-sel='sub-head-2']") %>% 
+    .[html_text2(.) == "Occupation"] %>% 
+    html_element(xpath = "following-sibling::text()") %>% 
+    html_text() %>% 
+    str_squish(),
+  social_link = card_nodes %>% 
+    html_element("a") %>% 
+    html_attr("href")
+) %>% 
+  mutate(season = "10")
 
-age_jobs_all_s6 <- c(extraagejob_s6, age_jobs_s6)
-
-names_s6 <- s6page %>%
-  html_elements(".e1l1jp228 h3") %>%
-  html_text2()
-
-s6cast <- data.frame(names_s6, age_jobs_all_s6)
-
-s6cast_final <- s6cast %>%
-  mutate(age_jobs_all_s6 = str_remove(age_jobs_all_s6,"Age and Occupation")) %>%
-  separate_wider_delim(age_jobs_all_s6, delim = ", ", names = c("age","job")) %>%
-  mutate(season = "6") %>%
-  rename(names = names_s6)
-
-# alt version
-s6cast_df <- tibble(
-  names = s6page %>%
-    html_elements(".e1l1jp228 h3") %>%
-    html_text2(),
-  age_jobs_all_s6 = age_jobs_all_s6,
-  ) %>%
-  mutate(age_jobs_all_s6 = str_remove(age_jobs_all_s6,"Age and Occupation")) %>%
-  separate_wider_delim(age_jobs_all_s6, delim = ", ", names = c("age","job")) %>%
-  mutate(season = "6")
-
-# grab all the instagram and tiktok links too
-social_links <- s6page %>%
-  html_elements(".e1l1jp226 a") %>%
-  html_attr("href")
 
 
 
